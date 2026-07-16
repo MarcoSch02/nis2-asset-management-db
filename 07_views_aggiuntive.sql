61b9ad30776a87836f0938119eda83dc919c0a4b
Marco Schiavone <marco.schiavone0256@gmail.com>
2026-07-16 11:19:49 +0200

    Delete 07_views_aggiuntive.sql

--- 07_views_aggiuntive.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW acn_asset_critici AS
-SELECT 
-    a.nome,
-    a.tipologia,
-    c.livello AS criticita,
-    a.descrizione
-FROM asset a
-JOIN criticita c ON a.criticita_id = c.id
-WHERE c.livello IN ('Alta', 'Molto Alta');
